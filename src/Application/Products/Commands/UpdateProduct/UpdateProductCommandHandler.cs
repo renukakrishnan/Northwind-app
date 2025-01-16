46ee39824ad3ac3ae0ae42bfab16e96bef8e15d3
NoReply Amazon <no-reply@amazon.com>
2025-01-16 21:09:28 +0000

    Revert "[Commit] Iteration 19 #errors = 2 <= 2."
    This reverts commit bb7908c48d9830c15300f489bd928d00a74644be.

--- src/Application/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
+++ src/Application/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,4 +1,4 @@
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Northwind.Application.Common.Exceptions;
@@ -7,7 +7,7 @@ using Northwind.Domain.Entities;
 
 namespace Northwind.Application.Products.Commands.UpdateProduct
 {
-    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, Unit>
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand>
     {
         private readonly INorthwindDbContext _context;
 
